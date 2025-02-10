98e7ae6a24a00b716d133e0ca477a485e17c8edb
User <User@DESKTOP-HLLTF6L>
2025-02-10 18:06:58 +0500

    - mobile kamchiliklar togrlandi

--- _next/static/chunks/app/layout-72f38aca50323dc4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5737:function(n,e,u){Promise.resolve().then(u.t.bind(u,5825,23))},5825:function(){}},function(n){n.O(0,[41,971,23,744],function(){return n(n.s=5737)}),_N_E=n.O()}]);
